ba9ea31c917dc00a7ff4defaaa4e5c308eb5c2bc
idev <insert@kakao.com>
2020-07-05 09:48:37 +0900

    Initial Commit

---
+++ Practice_TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice_TableView
+//
+//  Created by iDev0 on 2020/07/05.
+//  Copyright © 2020 Ju Young Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
